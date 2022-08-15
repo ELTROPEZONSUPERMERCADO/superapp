32d4ae701e0cf95b1a2adf62d29fc24a3d0b8a63
Agustin Sanchez <cuartitoup@gmail.com>
2022-08-15 18:08:33 -0300

    entrega1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
+        </p>
+         Supermercado El Tropezon
         </p>
         <a
           className="App-link"
